59d671a33d3a2d442d58676888c5b9ef40d4e282
scottviewzr <scott@viewzr.com>
2016-11-11 09:45:03 -0700

    Basic13

---
+++ Algorithms/chapter1/basic13/shiftArrayValues.js
@@ -0,0 +1,9 @@
+// Given an array, move all values forward by one
+// index, dropping the first and leaving a '0' value
+// at the end.
+
+function shiftArray(arr){
+	for(var i = 0; i < arr.length; i++){
+		s
+	}
+}
