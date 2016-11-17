5c26cd7b98e85cc8babd8c705b69f4d6dd17254c
scottviewzr <scott@viewzr.com>
2016-11-17 11:59:58 -0700

    sumToOne

---
+++ Algorithms/chapter2/clockHandAngles.js
@@ -0,0 +1,12 @@
+// Create clockHandAngles(seconds) that, given a number of seconds 
+// since 12:00:00, prints angles (in degrees) of the hour, minute and
+// seconds hands. As review, 360 degreed form a full rotation. 
+// For input of 3600 secs (equivalent to 1:00:00), print 
+// "Hour hand: 360 degs. Minute hand: 93 degs. Second hand: 180 degs".
+// Note: in the second example, the angle for the minute hand is not
+// simply 90 degs; it has advanced a bit further, because of the additional
+// 30 secs in that minute so far.
+
+function clockHandAngles(secs){
+	
+}
