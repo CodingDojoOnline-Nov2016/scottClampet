3f8fb6ae9e86526e3ee319812599c5ce61ae84d9
scottviewzr <scott@viewzr.com>
2016-12-01 11:02:05 -0700

    inital regex commit

--- python/fundamentals/regex.py
+++ python/fundamentals/regex.py
@@ -2,9 +2,9 @@
 # All words that contain a v
 # All words that contain a double-s
 # All words that end with an e
-# All words that contain an b, any character, then another b
-# All words that contain an b, at least one character, then another b
-# All words that contain an b, any number of characters (including zero), then another b
+# All words that contain a b, any character, then another b
+# All words that contain a b, at least one character, then another b
+# All words that contain a b, any number of characters (including zero), then another b
 # All words that include all five vowels in order
 # All words that only use the letters in regular expression (each letter can appear any number of times)
 # All words that contain a double letter
@@ -15,3 +15,13 @@ def get_matching_words(regex):
 	words = ["aimlessness", "assassin", "baby", "beekeeper", "belladonna", "cannonball", "crybaby", "denver", "embraceable", "facetious", "flashbulb", "gaslight", "hobgoblin", "iconoclast", "issue", "kebab", "kilo", "laundered", "mattress", "millennia", "natural", "obsessive", "paranoia", "queen", "rabble", "reabsorb", "sacrilegious", "schoolroom", "tabby", "tabloid", "unbearable", "union", "videotape"]
 
 	return [word for word in words if re.search(regex, word)]
+
+print get_matching_words('v')
+print get_matching_words('ss')
+print get_matching_words('e$')
+print get_matching_words('b.b')
+print get_matching_words('b.+b')
+print get_matching_words('b.*b')
+print get_matching_words('aeiou')
+print get_matching_words('[a-zA-z]')
+print get_matching_words(r'(.)\1')
