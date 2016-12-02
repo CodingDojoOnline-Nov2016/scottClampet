c7bd5d082fe66183f68e214270fb399211ca8da5
scottviewzr <scott@viewzr.com>
2016-12-01 17:11:04 -0700

    inital bubbleSort

--- python/fundamentals/bubbleSort.js
+++ python/fundamentals/bubbleSort.js
@@ -1,16 +1,20 @@
-
-arr = [6,5,3,1,8,7,2,4]
+// Example bubbleSort
+array = [6,5,3,1,8,7,2,4]
 
 function bubbleSort(arr){
-	for(var i = 0; i < arr.length - 1; i++){
-		if(arr[i] > arr[i + 1]){
-			temp = arr[i];
-			arr[i] = arr[i + 1];
-			arr[i + 1] = temp;
-		} else {
-			continue;
+	var swapped = true;
+	while(swapped){
+		swapped = false
+		for(var i = 0; i < arr.length - 1; i++){
+			if(arr[i] > arr[i + 1]){
+				temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				swapped = true;
+			}
 		}
 	}
 	return arr;
 }
-console.log(bubbleSort(arr));
+console.log(bubbleSort(array))
+	
