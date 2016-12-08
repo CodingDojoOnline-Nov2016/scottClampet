8ea1cce21e429a0dd18e1a525d1a0cc307261c47
scottviewzr <scott@viewzr.com>
2016-12-08 15:03:09 -0700

    form_test added sessions

--- python/flask/form_test/server.py
+++ python/flask/form_test/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'ThisIsSecret'
 @app.route('/') #used to render our form
 def index():
 	return render_template('index.html')
@@ -8,8 +9,12 @@ def index():
 def create_users(): # notice how we defined which HTTP methods are allowed by this route
 	print "Get Post Info"
 
-	name = request.form['name']
-	email = request.form['email']
+	session['name'] = request.form['name']
+	session['email'] = request.form['email']
 
-	return redirect('/')
+	return redirect('/show')
+@app.route('/show')
+def show_user():
+	return render_template('user.html')
+
 app.run(debug=True) #runs our server
