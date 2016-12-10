28bd57711856edf13fe1777de1b8398aecb48885
scottviewzr <scott@viewzr.com>
2016-12-10 14:58:57 -0700

    basic_val/dojo_survey validation

--- python/flask/dojo_survey/server.py
+++ python/flask/dojo_survey/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
+app.secret_key = "ThisIsSecret"
 
 @app.route('/')
 def index():
@@ -13,6 +14,13 @@ def survey_result():
 	location = request.form['location']
 	lang = request.form['lang']
 	comment = request.form['comment']
+	if(len(name) < 1):
+		flash("You must enter a valid name!")
+		return redirect('/')
+	if(len(comment) > 120):
+		flash("Sorry, your comment is more than 120 characters long. Don't do dat!")
+		return redirect('/')
+		
 
 	return render_template('result.html', info=request.form)
 
