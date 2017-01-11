2409d9693c2c96dd0aa6dc432798003d3c4915b7
scottviewzr <scott@viewzr.com>
2017-01-11 16:22:05 -0500

    up to random_word assignment

--- python_stack/django/main/apps/vinmyMVC/views.py
+++ python_stack/django/main/apps/vinmyMVC/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
@@ -9,3 +9,14 @@ def show(request):
 	print 'Users Got iT!'
 	print (request.method)
 	return render(request, 'vinmyMVC/show_users.html')
+
+def create(request):
+	print 'Method = ',request.method
+	if request.method == "POST":
+		print ('*'*50)
+		print (request.POST)
+		print ('*'*50)
+		request.session['name'] = request.POST['first_name']
+		return redirect('/')
+	else:
+		return redirect('/')
